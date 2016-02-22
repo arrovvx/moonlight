3dc32a535f64b10076141c2cab3134d5f181a223
Arrow IBM <arrovvx@gmail.com>
2016-02-22 18:45:59 -0500

    structured the about page

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var navListObj = {
+	index: "Home", 
+	about: "About", 
+	contact: "Contact"
+}
 
 /* GET home page. */
+router.get('/index', function(req, res, next) {
+	
+	res.render('index', { title: "Owen's Site", navBarToggle: false, navListObj: navListObj});
+});
+
 router.get('/', function(req, res, next) {
 	
-	navListObj = {
-		about: "About", 
-		blog: "Blog", 
-		contact: "Contact"
-	}
 	res.render('index', { title: "Owen's Site", navBarToggle: false, navListObj: navListObj});
+});
+
+router.get('/about', function(req, res, next) {
+	
+	res.render('about', { title: "About Owen", navBarToggle: true, navListObj: navListObj});
 });
 
 module.exports = router;
