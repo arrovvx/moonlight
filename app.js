536b6de90a551308d8fd615c129ddf0308754f34
Owen Yeh <arrovvx@gmail.com>
2017-10-26 13:35:06 -0400

    finished new site

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var validator = require('express-validator');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -15,21 +14,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/index.html', routes);
-app.use('/users', users);
+app.use('/about', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('404 Page Not Found');
   err.status = 404;
   next(err);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var key1 = JSON.parse(fs.readFileSync('key1.json', 'utf8'));
+var key2 = JSON.parse(fs.readFileSync('key2.json', 'utf8'));
+var validator = require('express-validator');
+var crypto = require('crypto');
+
+var sha512 = function(password, salt){
+    var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+    hash.update(password);
+    var value = hash.digest('hex');
+    return value;
+};
 
 /* GET home page. */
 router.get('/index', function(req, res, next) {
@@ -12,9 +24,28 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: "Owen's Site", navBarToggle: true});
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/contact', function(req, res, next) {
 	
-	res.render('about', { title: "About Owen", navBarToggle: true});
+	res.render('contact', { title: "Owen's contacts", navBarToggle: true});
+});
+
+router.post('/about',function(req, res, next) {
+		req.checkBody('password', 'Invalid password').isAlpha();
+		
+		var errors = req.validationErrors();
+		console.log(errors);
+		if (errors) {
+			res.status(400).send('Invalid Inputs');
+		} else {
+			//check both password to see if there is a match
+			if(sha512(req.body.password, key1.salt) === key1.passwordHash
+				|| sha512(req.body.password, key2.salt) === key2.passwordHash){
+					
+				res.render('about', { title: "About Owen", navBarToggle: true});
+			} else {
+				res.status(401).send('Invalid Password');
+			}
+		}
 });
 
 module.exports = router;
